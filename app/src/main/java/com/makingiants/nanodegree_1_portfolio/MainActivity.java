e28d053c1432433e49639981fcec89a9f8de8113
Daniel Gomez Rico <danielgomezrico@gmail.com>
2016-11-09 22:06:47 -0500

    Add activity with buttons

--- app/src/main/java/com/makingiants/nanodegree_1_portfolio/MainActivity.java
+++ app/src/main/java/com/makingiants/nanodegree_1_portfolio/MainActivity.java
@@ -1,7 +1,11 @@
 package com.makingiants.nanodegree_1_portfolio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main_activity);
+    ButterKnife.bind(this);
+  }
+
+  @OnClick({
+      R.id.popularMoviesButton, R.id.stockHawkButton, R.id.buildItBiggerButton,
+      R.id.makeYourAppMaterialButton, R.id.goUbiquitousButton,
+      R.id.capstoneButton,
+  })
+  public void onButtonClick(Button button) {
+    Toast.makeText(this,
+        getString(R.string.this_button_will_launch, button.getText()),
+        Toast.LENGTH_SHORT)
+        .show();
   }
 }
